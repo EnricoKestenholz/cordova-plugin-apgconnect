30fd17ef5a223c498b283aeba05588a3f6dcb8c5
EnricoKestenholz <enrico@tio.ch>
2018-11-13 09:22:15 +0100

    Initial commit

---
+++ src/android/apgconnect.java
@@ -0,0 +1,11 @@
+package podspec;
+
+import org.apache.cordova.CordovaPlugin;
+
+/**
+ * Apache Cordova plugin for Windows Azure Notification Hub
+ */
+public class apgconnect extends CordovaPlugin {
+
+
+}
---
+++ src/ios/apgconnect.h
@@ -0,0 +1,6 @@
+#import <Cordova/CDVPlugin.h>
+
+@interface apgconnect : CDVPlugin <NSXMLParserDelegate>
+
+
+@end
